9f5c5d5de2c594a1af2d4e954f0435dfb860133a
Alan <allan_net@live.com>
2022-03-19 17:32:52 -0300

    static to dynamic database

---
+++ helpers/api-util.js
@@ -0,0 +1,17 @@
+export async function getAllEvents() {
+  const response = await fetch(process.env.DATABASE_URL);
+  const data = await response.json();
+  const events = [];
+  for (const key in data) {
+    events.push({
+      id: key,
+      ...data[key],
+    });
+  }
+  return events;
+}
+
+export async function getFeaturedEvents() {
+  const allEvents = await getAllEvents();
+  return allEvents.filter((event) => event.isFeatured);
+}
